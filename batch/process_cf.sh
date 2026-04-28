063969a609a0fb9c124921dc497bceab11f0053b
colin.lenoble0@gmail.com <lenoblec@ruche01.cluster>
2026-04-28 17:49:58 +0200

    new_commit with downscaling hourly working

--- batch/process_cf.sh
+++ batch/process_cf.sh
@@ -15,9 +15,37 @@ module load anaconda3/2023.09-0/none-none
 source activate /gpfs/workdir/shared/juicce/envs/xenv
 
 # Run python script
-python ../code/bias_correction_qdm.py \
-    --era5-dir /gpfs/workdir/shared/juicce/RE_Colin/India/renewable_india/data/raw/era5_daily/ \
-    --cmip-dir /gpfs/workdir/shared/juicce/RE_Colin/India/renewable_india/data/raw/CanESM5/ \
-    --env-dir /gpfs/workdir/shared/juicce/envs/xenv \
-    --out-dir /gpfs/workdir/shared/juicce/RE_Colin/India/renewable_india/data/proc/CanESM5/ \
-    --nquantile 25
+#python ../code/bias_correction_qdm.py \
+#    --era5-dir /gpfs/workdir/shared/juicce/RE_Colin/India/renewable_india/data/raw/era5_daily/ \
+#    --cmip-dir /gpfs/workdir/shared/juicce/RE_Colin/India/renewable_india/data/raw/CanESM5/ \
+#    --env-dir /gpfs/workdir/shared/juicce/envs/xenv \
+#    --out-dir /gpfs/workdir/shared/juicce/RE_Colin/India/renewable_india/data/proc/CanESM5/ \
+#    --nquantile 25
+    
+python ../code/downscale_hourly.py fit \
+    --era5-nc  ../data/raw/era5/era5_india_2025_hourly.nc \
+    --gcm-grid   ../data/proc/CanESM5/tas_CanESM5_historical_bc.nc \
+    --out-library ../data/proc/era5/diurnal_library_CanESM5.nc \
+    --env-dir /gpfs/workdir/shared/juicce/envs/xenv 
+# 2 – Apply (per SSP):
+python ../code/downscale_hourly.py apply \
+    --library  ../data/proc/era5/diurnal_library_CanESM5.nc \
+    --bc-dir   ../data/proc/CanESM5 \
+    --gcm      CanESM5 --run r10i1p1f1 \
+    --ssps     historical ssp245 ssp585 \
+    --out-dir  ../data/proc/cmip6_hourly \
+    --doy-window 30
+
+
+python ../code/compute_cf.py \
+    --bc-dir      ../data/proc/cmip6_bc \
+    --hourly-dir  ../data/proc/cmip6_hourly \
+    --cmip-dir    ../data/raw/CanESM5 \
+    --shapefile   ../INDIA_STATES.geojson \
+    --out-dir     ../data/proc/cmip6_bc \
+    --gcm         CanESM5 \
+    --run         r10i1p1f1 \
+    --ssps        ssp245 ssp585 \
+    --train-start 1980-01-01 \
+    --train-end   2010-12-31 \
+    --region-col  STNAME_SH
